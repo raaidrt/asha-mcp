4cd938d9156122a4e6614920e81dd9c1a7f6bbac
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2025-08-08 14:13:05 +0600

    changes

--- src/asha/__init__.py
+++ src/asha/__init__.py
@@ -1,9 +1,16 @@
-from mcp.server.fastmcp import FastMCP
+from typing import Any
+from mcp.server.fastmcp import FastMCP, Image
 
 import chess
+import chess.svg
 from pathlib import Path
 
 import stockfish
+
+from PIL import Image as PILImage
+
+import time
+import os
 
 stockfish_path = Path(__file__).parent / 'stockfish_bin'
 stockfish_binary = next(stockfish_path.iterdir())
@@ -13,16 +20,22 @@ path = str(stockfish_binary)
 engine = stockfish.Stockfish(str(stockfish_binary))
 
 
+def get_eval_string(eval: dict[Any, Any]) -> str:
+    if eval['type'] == 'cp':
+        return f'CP[{eval["value"]}]'
+    return f'Mate[{eval["value"]}]'
+
+
 @mcp.tool()
 async def eval_next_moves(board_fen: str) -> str:
     board = chess.Board(board_fen)
     next_moves = board.generate_legal_moves()
-    result = {}
+    result: dict[str, str] = {}
     for move in next_moves:
         board.push(move)
         engine.set_fen_position(board.fen())
         engine.set_depth(10)
-        result[board.fen()] = engine.get_evaluation() / 100
+        result[board.fen()] = get_eval_string(engine.get_evaluation())
         board.pop()
 
     return str(result)
@@ -39,12 +52,22 @@ async def get_next_board_state(board_fen: str, move_san: str) -> str:
 async def get_evaluation(board_fen: str) -> str:
     board = chess.Board(board_fen)
     engine.set_fen_position(board.fen())
-    return str(engine.get_evaluation() / 100)
+    return get_eval_string(engine.get_evaluation())
 
 
 @mcp.tool()
 async def start_board() -> str:
     return chess.Board().fen()
+
+
+@mcp.tool()
+async def board_image(board_fen: str) -> Image:
+    svg_source = chess.svg.board(chess.Board(board_fen))
+    filename = Path(f'/tmp/_asha_stockfish_{time.time()}_{os.getpid()}.svg')
+    with open(filename, 'w') as f:
+        f.write(svg_source)
+    img = PILImage.open(filename)
+    return Image(data=img.tobytes(), format='png')
 
 
 def main():
